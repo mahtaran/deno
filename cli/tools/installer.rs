64b3a2d9f81aaf2679efd205fc3564dd5dfc8030
Luka Leer <luka.leer@gmail.com>
2024-12-26 03:53:32 +0100

    fix(cli): do not create `bin` folder within `DENO_INSTALL_ROOT`
    As can be seen in the documentation, `DENO_INSTALL_ROOT` is meant to be the folder wherein Deno will install executables, with the default being `$HOME/.deno/bin`.
    
    However, the current behaviour of the `install` and `uninstall` commands, if `DENO_INSTALL_ROOT` is set, is to still append "bin" to the path. This leads to executables being installed in a folder like `$HOME/.deno/bin/bin`, which is obviously not the intended behaviour.
    
    This commit removes this appending. Do note that this is a breaking change in the behaviour of the `--root` flag, as previously it referred to a folder wherein the `bin` folder was located, but now it refers to the `bin` folder itself. For example: `deno install --root /foo/bar test.ts` would previously install the executable in `/foo/bar/bin/test`, but now it will install it in `/foo/bar/test`.

--- cli/tools/installer.rs
+++ cli/tools/installer.rs
@@ -136,6 +136,7 @@ fn get_installer_root() -> Result<PathBuf, io::Error> {
         )
       })?;
   home_path.push(".deno");
+  home_path.push("bin");
   Ok(home_path)
 }
 
@@ -213,12 +214,11 @@ pub async fn uninstall(
   };
 
   let cwd = std::env::current_dir().context("Unable to get CWD")?;
-  let root = if let Some(root) = uninstall_flags.root {
+  let installation_dir = if let Some(root) = uninstall_flags.root {
     canonicalize_path_maybe_not_exists(&cwd.join(root))?
   } else {
     get_installer_root()?
   };
-  let installation_dir = root.join("bin");
 
   // ensure directory exists
   if let Ok(metadata) = fs::metadata(&installation_dir) {
@@ -473,12 +473,11 @@ async fn resolve_shim_data(
   install_flags_global: &InstallFlagsGlobal,
 ) -> Result<ShimData, AnyError> {
   let cwd = std::env::current_dir().context("Unable to get CWD")?;
-  let root = if let Some(root) = &install_flags_global.root {
+  let installation_dir = if let Some(root) = &install_flags_global.root {
     canonicalize_path_maybe_not_exists(&cwd.join(root))?
   } else {
     get_installer_root()?
   };
-  let installation_dir = root.join("bin");
 
   // Check if module_url is remote
   let module_url = resolve_url_or_path(&install_flags_global.module_url, &cwd)?;
@@ -872,7 +871,7 @@ mod tests {
         module_url: "http://localhost:4545/echo_server.ts".to_string(),
         args: vec![],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: false,
       },
     )
@@ -1162,6 +1161,7 @@ mod tests {
   #[tokio::test]
   async fn install_npm_lockfile_default() {
     let temp_dir = canonicalize_path(&env::temp_dir()).unwrap();
+    let bin_dir = temp_dir.join("bin");
     let shim_data = resolve_shim_data(
       &HttpClientProvider::new(None, None),
       &Flags {
@@ -1175,14 +1175,14 @@ mod tests {
         module_url: "npm:cowsay".to_string(),
         args: vec![],
         name: None,
-        root: Some(temp_dir.to_string_lossy().to_string()),
+        root: Some(bin_dir.to_string_lossy().to_string()),
         force: false,
       },
     )
     .await
     .unwrap();
 
-    let lock_path = temp_dir.join("bin").join(".cowsay.lock.json");
+    let lock_path = bin_dir.join(".cowsay.lock.json");
     assert_eq!(
       shim_data.args,
       vec![
@@ -1249,7 +1249,7 @@ mod tests {
         module_url: local_module_str.to_string(),
         args: vec![],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: false,
       },
     )
@@ -1279,7 +1279,7 @@ mod tests {
         module_url: "http://localhost:4545/echo_server.ts".to_string(),
         args: vec![],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: false,
       },
     )
@@ -1300,7 +1300,7 @@ mod tests {
         module_url: "http://localhost:4545/cat.ts".to_string(), // using a different URL
         args: vec![],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: false,
       },
     )
@@ -1322,7 +1322,7 @@ mod tests {
         module_url: "http://localhost:4545/cat.ts".to_string(), // using a different URL
         args: vec![],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: true,
       },
     )
@@ -1353,7 +1353,7 @@ mod tests {
         module_url: "http://localhost:4545/cat.ts".to_string(),
         args: vec![],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: true,
       },
     )
@@ -1383,7 +1383,7 @@ mod tests {
         module_url: "http://localhost:4545/echo_server.ts".to_string(),
         args: vec!["\"".to_string()],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: false,
       },
     )
@@ -1424,7 +1424,7 @@ mod tests {
         module_url: local_module_str.to_string(),
         args: vec![],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: false,
       },
     )
@@ -1470,7 +1470,7 @@ mod tests {
         module_url: "http://localhost:4545/cat.ts".to_string(),
         args: vec![],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: true,
       },
     )
@@ -1513,7 +1513,7 @@ mod tests {
         module_url: file_module_string.to_string(),
         args: vec![],
         name: Some("echo_test".to_string()),
-        root: Some(temp_dir.path().to_string()),
+        root: Some(bin_dir.to_string()),
         force: true,
       },
     )
@@ -1570,7 +1570,7 @@ mod tests {
       UninstallFlags {
         kind: UninstallKind::Global(UninstallFlagsGlobal {
           name: "echo_test".to_string(),
-          root: Some(temp_dir.path().to_string()),
+          root: Some(bin_dir.to_string()),
         }),
       },
     )
